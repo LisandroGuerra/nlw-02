fea2c8a1044832b70b0ef11dcc6be63cc4c1a83d
LisandroGuerra <lisandro.digital@gmail.com>
2020-08-05 00:02:01 -0300

    web component TeacherItem

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 
 
 
+
 function TeacherList() {
     return (
-        <div id="page-teacher-list" className="container">
+        <div id="page-teachers-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
                     <div className="input-block">
@@ -27,6 +29,14 @@ function TeacherList() {
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+            </main>
         </div>
     )
 }
