6f1e778f6f7c65d0798d2af61b409ee318259323
LisandroGuerra <lisandro.digital@gmail.com>
2020-08-05 13:28:37 -0300

    server README databse and migrations

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,23 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
+app.use(express.json());
+
+/* app.get('/users', (request, response) => {
     const users = [
         {name: 'Lisandro', age: 49},
         {name: 'Cida', age: 51},
-        {name: 'Lica', age: 21},
+        {name: 'Lilica', age: 21},
+    ]; */
+
+app.post('/users', (request, response) => {
+    console.log(request.body);
+
+
+    const users = [
+        {name: 'Lisandro', age: 49},
+        {name: 'Cida', age: 51},
+        {name: 'Lilica', age: 21},
     ];
 
     return response.json(users);
