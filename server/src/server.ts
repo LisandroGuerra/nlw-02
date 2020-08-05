9447f0e7a8363e34ab853547e16d4d0a21605d7f
LisandroGuerra <lisandro.digital@gmail.com>
2020-08-05 15:43:38 -0300

    server refactoring

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-/* app.get('/users', (request, response) => {
-    const users = [
-        {name: 'Lisandro', age: 49},
-        {name: 'Cida', age: 51},
-        {name: 'Lilica', age: 21},
-    ]; */
-
-app.post('/users', (request, response) => {
-    console.log(request.body);
-
-
-    const users = [
-        {name: 'Lisandro', age: 49},
-        {name: 'Cida', age: 51},
-        {name: 'Lilica', age: 21},
-    ];
-
-    return response.json(users);
-});
+app.use(routes);
 
 
 app.listen(3333);
