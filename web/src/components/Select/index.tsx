8a513bb20fe087c3c46e1569e0eb1ef7b2a09e16
LisandroGuerra <lisandro.digital@gmail.com>
2020-08-06 10:11:32 -0300

    web component Select

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -3,6 +3,7 @@ import React, { SelectHTMLAttributes } from 'react';
 import './styles.css';
 
 
+
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
     name: string;
     label: string;
@@ -26,6 +27,5 @@ const Select: React.FC<SelectProps> = ({name, label, options, ...rest}) => {
         </div>
     );
 }
-
 
 export default Select;
