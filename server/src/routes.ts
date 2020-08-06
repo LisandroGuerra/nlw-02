703df22500bbac44b5bfad8d378f5d0f07389518
LisandroGuerra <lisandro.digital@gmail.com>
2020-08-05 21:01:43 -0300

    server controllers

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,59 +1,17 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHoursToMinutes';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsControler from './controllers/ConnectionsController';
+
 
  const routes = express.Router();
+ const classesControllers = new ClassesController();
+ const connectionsControllers = new ConnectionsControler();
 
- interface ScheduleItem {
-     week_day: number;
-     from: string;
-     to: string;
- }
 
-routes.get('/', (request, response) => {
-    return response.json({message: 'Hello World!'});
-});
+routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
 
-routes.post('/classes', async (request, response) => {
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = request.body;
-
-   const insertedUsersIds = await db('users').insert({
-       name,
-       avatar,
-       whatsapp,
-       bio
-   });
-
-   const user_id = insertedUsersIds[0];
-
-   const insertedClassesIds = await db('classes').insert({
-       subject,
-       cost,
-       user_id
-   });
-
-   const class_id = insertedClassesIds[0];
-
-   const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-       return {
-           class_id,
-           week_day: scheduleItem.week_day,
-           from: convertHourToMinutes(scheduleItem.from),
-           to: convertHourToMinutes(scheduleItem.to)
-       };
-   });
-
-   const insertedSchedules = await db('class_schedules').insert(classSchedule);
-
-    return response.send();
-});
+routes.get('/connections', connectionsControllers.index);
+routes.post('/connections', connectionsControllers.create);
 
 export default routes;
